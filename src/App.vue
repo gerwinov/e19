62d6c394efb369fce0a487ad99d07f78aa956064
Gerwin Overeem <gerwin@overeem.io>
2020-04-18 10:37:02 +0200

    Prevent scrolling body

--- src/App.vue
+++ src/App.vue
@@ -92,17 +92,17 @@ export default {
   box-sizing: border-box;
 }
 
-html {
-    height  : 100%;
-    overflow: hidden;
+html,
+body {
+  position: fixed;
+  overflow: hidden;
 }
 
 body {
-  width: 100vw;
+  width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
-  overflow: auto;
 }
 
 h1 {
